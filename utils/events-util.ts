3320dfb76f3dd81f3c98a33eb1b09a720e660af4
Xuan <48014272+su-xuan@users.noreply.github.com>
2022-02-14 12:49:28 +0100

    getStaticProps fetches from database directly

--- utils/events-util.ts
+++ utils/events-util.ts
@@ -1,24 +1,56 @@
-import { server } from "../config";
+import { MongoClient, ObjectId } from "mongodb";
+import connectDatabase from "./dbConnect";
 
 export const getAllEvents = async () => {
-  let events;
-  await fetch(`${server}/api/get-all/events`)
-    .then((res) => res.json())
-    .then((data) => (events = data));
-  return events;
+  const getDocument = async (client: MongoClient) => {
+    const db = client.db("meetup");
+    const collectionDb = db.collection("events");
+    const documents = await collectionDb.find().toArray();
+    return documents;
+  };
+
+  const client = await connectDatabase();
+  const events = await getDocument(client);
+  
+  return events.map((event) => {
+    return {
+      ...event,
+      _id: event._id.toString()
+    }
+  });
 };
 
 export const getEventsByDistrict = async (district: string) => {
-  let events;
-  await fetch(`${server}/api/find/events/district/${district}`)
-    .then((res) => res.json())
-    .then((data) => (events = data));
-  return events;
+  const getDocument = async (client: MongoClient) => {
+    const db = client.db("meetup");
+    const collectionDb = db.collection("events");
+    const documents = await collectionDb.find({district: district}).toArray();
+    return documents;
+  };
+
+  const client = await connectDatabase();
+  const events = await getDocument(client);
+  
+  return events.map((event) => {
+    return {
+      ...event,
+      _id: event._id.toString()
+    }
+  });
 };
 
 export const getEventById = async (eventId: string) => {
-   let events
-   await fetch(`${server}/api/find/events/_id/${eventId}`)
-   .then((res) => res.json())
-   .then((data) => (events = data));
- return events;}
+  const getDocument = async (client: MongoClient) => {
+    const db = client.db("meetup");
+    const collectionDb = db.collection('events');
+    const object = await collectionDb.findOne({'_id': new ObjectId(eventId)});
+    return object;
+  };
+  const client = await connectDatabase();
+  const event = await getDocument(client)
+
+  return {
+    ...event,
+    _id: event?._id.toString()
+  }
+}
